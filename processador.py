8062a69994d1cf7dbc222c408e4bfc98c6778734
Ian Baldo <ianblopes@gmail.com>
2022-10-31 17:12:45 -0300

    Added throughtput and lead-time + pdf generation

--- processador.py
+++ processador.py
@@ -4,6 +4,8 @@ from ApiConnector import ApiConnector
 from datetime import datetime, timedelta
 from dateutil import parser
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+import numpy as np
 
 configFile = open("./config.json")
 config = json.load(configFile)
@@ -43,23 +45,78 @@ def dateNow():
 
 def calcCycleTimeAvg(data):
     for status in data:
-        data[status] = (sum(data[status]) / len(data[status]))
+        if len(data[status]) == 0:
+            data[status] = 0
+            continue
+        data[status] = round((sum(data[status]) / len(data[status])), 1)
     return data
 
-def plotCycleTime(cycle_time):
-    fig, ax = plt.subplots()
+def calcLeadTime(cycle_time_avg):
+    progress_status_list = ["Doing", "In Code Review", "Ready QA", "Em QA", "To Review"]
+    lead_time_avg = 0
+    for status in cycle_time_avg:
+        if status in progress_status_list:
+            lead_time_avg += cycle_time_avg[status]
+            
+    return round(lead_time_avg, 1)
+
+def plotCycleTime(cycle_time, ax=None):
+    ax = ax or plt.gca()
 
     status = cycle_time.keys()
     values = cycle_time.values()
 
-    ax.bar(status, values)
+    rects = ax.bar(status, values)
+    ax.bar_label(rects, padding=3)
 
-    ax.set_ylabel('Tempo em Dias (avg)')
-    ax.set_title('Cycle Time')
+    ax.set_ylabel('Dias')
+    ax.set_title('Cycle Time(Avg)')
 
-    plt.show()
+    plt.plot()
 
-f = open("output.json", "w")
+def plotThroughtputAndLeadTime(throughput, lead_time):
+    plt.text(0.18, 1, "Throughput", size=15)
+    plt.text(0.35, 0.8, throughput, size=15, bbox=dict(boxstyle="round"))
+
+    plt.text(0.2, 0.5, "Lead Time", size=15)
+    plt.text(0.3, 0.3, lead_time, size=15, bbox=dict(boxstyle="round"))
+
+def plot2Pdf(pdf_title, cycle_time_avg, throughput):
+    deepgreen = '#8c9344'
+    lightbeige = '#eef1d4'
+    medgreen = '#a2a963'
+    darkbrown = '#242112'
+
+    with PdfPages("%s.pdf" % pdf_title) as pdf:
+        # figsize of A4 paper
+        fig = plt.figure(figsize=(8.3, 11.7), facecolor=deepgreen)
+        rows = 3
+        cols = 2
+        grid = plt.GridSpec(rows, cols, wspace=0.2, hspace=0.3)
+
+        col = darkbrown
+        fig_ax1 = fig.add_subplot(grid[0, 0], facecolor=medgreen)
+        fig_ax1.set_axis_off()
+        plotThroughtputAndLeadTime(throughput, calcLeadTime(cycle_time_avg))
+
+        desc1 = 'A short description here \n line break continues \n the interesting information.'
+        desc2 = '\n \n We can fill the space \n so that it looks nicely placed.'
+        desc3 = '\n \n ~ bullet 1 \n ~ bullet 2 \n ~ bullet 3'
+        fig.text(1/cols+0.025, 2/rows+0.03, desc1+desc2+desc3, color=darkbrown) # horizontally 1/2 halfway from left, vertically 2/3 from bottom, 0,0 is bottom left, 1,1 is top right
+        
+
+        fig_ax3 = fig.add_subplot(grid[1,:2])
+        plotCycleTime(cycle_time_avg)
+        
+        fig_ax4 = fig.add_subplot(grid[2, 0])
+        fig_ax5 = fig.add_subplot(grid[2, 1])
+
+        fig.suptitle('Métricas Off-Road', fontsize=38, color=lightbeige, family='monospace')
+        
+        pdf.savefig(facecolor=deepgreen)
+        plt.close()
+
+# f = open("output.json", "w")
 
 # Open file 
 with open('data.csv') as file_obj:
@@ -77,7 +134,7 @@ with open('data.csv') as file_obj:
         "Doing" : [],
         "In Code Review" : [],
         "Ready QA" : [],
-        "QA Validation" : [],
+        "Em QA" : [],
         "To Review" : [],
         "PO Validation" : [],
     }
@@ -112,5 +169,5 @@ with open('data.csv') as file_obj:
         print("Processando %s: %s %s" % (processados, issue["status"], issue["last_status_change"]))
         processados += 1
 print("Throughput: %s" % throughput)
-plotCycleTime(calcCycleTimeAvg(cycle_time_data))
-f.close()
+plot2Pdf('metricas off-road', calcCycleTimeAvg(cycle_time_data), throughput)
+# f.close()
